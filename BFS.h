ac96aac3b2e12257b24361b46df07f5791ab0cae
shakarb <shakarbili@gmail.com>
2020-01-19 21:29:05 +0200

    BFS implementation was added, backtrace is missing

--- BFS.h
+++ BFS.h
@@ -8,9 +8,34 @@
 
 template <typename S, typename T>
 class BFS : public Searcher<S,T>{
-    virtual S search(/*Searchable<T> s*/) {
+    virtual S search(Searchable<T> *searchable) {
+        vector<State<T>> queue;
+        map<State<T>, bool> visited;
+        State<T> *prev = searchable->getInitialState();
+        queue.push_back(prev);
+        visited.insert(prev, true);
+
+        while (!queue.empty()) {
+            State<T> *state = queue.front();
+            queue.pop_back();
+            this->evaluatedNodes++;
+            if (searchable->isGoalState(state)) {
+                //back trace
+                string solution = "";
+                return solution;
+            }
+            vector<State<Point *>> successors = searchable->getAllPossisbleStates(state);
+            for (auto s:successors) {
+                s.setCameFrom(state);
+                if (s.getCost() != -1 && visited.find(s) == visited.end()) {
+                    queue.push_back(s);
+                    visited.insert(s, true);
+                }
+            }
+
+        }
+        return "Couldn't find a solution";
     }
 };
-
 
 #endif //MILESTONE2_BFS_H
